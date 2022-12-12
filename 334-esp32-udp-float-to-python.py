3a696b1fb286a1de309a334eee312664372adcd6
risxyang <christine.yang@yale.edu>
2022-12-12 01:08:24 -0500

    udp mpu6050 working

--- 334-esp32-udp-float-to-python.py
+++ 334-esp32-udp-float-to-python.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 UDP_IP = "172.29.21.183"
 UDP_PORT = 8092
@@ -9,4 +10,6 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)
-    print ("Message: ", data.decode("ASCII"))
+    content = data.decode("ASCII")
+    os.system('echo '+ content + '> wifiread')
+    print ("Message: ", content)
